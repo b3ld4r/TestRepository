66554d7811347da120986d15f196683367f30268
Adrián <beldar.5.ou@gmail.com>
2018-08-03 09:03:42 +0200

    BLD-2: - Transformación a proyecto Maven. - Inclusión de pom principal de proyecto.
    Signed-off-by: Adrián Álvarez <aalvarezr@be-sec.net>

---
+++ README.md
@@ -0,0 +1,2 @@
+# TestRepository
+Repositorio de pruebas donde se subirá algún proyecto para realizar pruebas con GIT.
---
+++ src/main/java/es/testrepository/Main.java
@@ -0,0 +1,8 @@
+package es.testrepository;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
