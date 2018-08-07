7fee5588defea59a706ff2eed3bbfe119bf733c7
Adrián <beldar.5.ou@gmail.com>
2018-08-07 11:04:31 +0200

    Update method main.

--- src/main/java/es/testrepository/Main.java
+++ src/main/java/es/testrepository/Main.java
@@ -3,6 +3,17 @@ package es.testrepository;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+        int x = 0, y = 0;
+
+        for (int i = 10; i>x; i--) {
+            System.out.println(i-y);
+            y++;
+        }
+
+        System.out.println("Test finished!");
+
     }
 }
